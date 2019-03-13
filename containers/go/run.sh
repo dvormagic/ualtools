d1de84e32d84d288980bc3671e81cbecec49161f
dvormagic <david.altipla@gmail.com>
2019-03-13 21:03:55 +0100

    Add provisional containers and docker-compose.

---
+++ containers/go/run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -eu
+
+if [[ -z $WORKDIR ]]; then
+  APP=$SERVICE
+else
+  APP=$(basename $WORKDIR)
+fi
+
+cd /workspace/$WORKDIR
+
+echo """
+**/*.go /workspace/pkg/**/*.go {
+  prep: go install ./cmd/$APP
+}
+/go/bin/$APP {
+  daemon +sigterm: $APP
+}
+""" > /tmp/modd.conf
+
+modd -f /tmp/modd.conf
