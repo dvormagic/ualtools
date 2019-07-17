0ec1738b54e53afc4597588148a37164f575f83c
dvormagic <dvormagic@gmail.com>
2019-07-17 15:00:30 +0200

    Change storage ualtools binary endpoint.

--- infra/install/install.sh
+++ infra/install/install.sh
@@ -5,7 +5,7 @@ set -eu
 mkdir -p ~/bin
 source ~/.bashrc
 
-curl https://ualtools.io/bin/ualtools > ~/bin/ualtools
+curl https://storage.googleapis.com/ualtools/bin/ualtools > ~/bin/ualtools
 chmod +x ~/bin/ualtools
 
 ualtools pull
